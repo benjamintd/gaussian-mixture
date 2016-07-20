dd966f30c80925bd96979ba329d3957c6635a2ec
Benjamin <btd@novavia.us>
2016-07-20 13:49:06 -0700

    Add score sample method and test.

--- index.js
+++ index.js
@@ -7,22 +7,24 @@ module.exports = GMM
 
 function GMM (nComponents, weights, means, vars) {
   // nComponents: number of components in the mixture
-  // weights: array of weights of each component in the mixture. Must sum to 1.
-  // means: array of means of each component.
-  // vars: array of variances of each component.
+  // weights: array of weights of each component in the mixture, must sum to 1
+  // means: array of means of each component
+  // vars: array of variances of each component
   this.nComponents = nComponents
   this.weights = weights === undefined ? _.range(nComponents).map(function () { return 1 / nComponents }) : weights
   this.means = means === undefined ? _.range(nComponents) : means
   this.vars = vars === undefined ? _.range(nComponents).map(function () { return 1 }) : vars
+  if (nComponents !== this.weights.length ||
+    nComponents !== this.means.length ||
+    nComponents !== this.vars.length) {
+    throw new Error('weights, means and vars must have nComponents elements.')
+  }
 }
 
 GMM.prototype.sample = function (nSamples) {
+  // nSamples: integer
   var samples = []
-  // generate gaussian models
-  var gaussians = []
-  for (var k = 0; k < this.nComponents; k++) {
-    gaussians.push(gaussian(this.means[k], this.vars[k]))
-  }
+  var gaussians = this._gaussians()
   // generate samples
   for (var i = 0; i < nSamples; i++) {
     var r = Math.random()
@@ -34,4 +36,29 @@ GMM.prototype.sample = function (nSamples) {
     samples.push(gaussians[n].ppf(Math.random()))
   }
   return samples
+}
+
+GMM.prototype.scoreSamples = function (data) {
+  // data: array of floats representing the samples to score under the model
+}
+
+GMM.prototype.scoreSample = function (x) {
+  // x: a float representing a single datapoint
+  var probs = []
+  var gaussians = this._gaussians()
+  for (var i = 0; i < this.nComponents; i++) {
+    probs.push(gaussians[i].pdf(x))
+  }
+  var sum = probs.reduce(function (a, b) { return a + b })
+  return probs.map(function (a) {
+    return a / sum
+  })
+}
+
+GMM.prototype._gaussians = function () {
+  var gaussians = []
+  for (var k = 0; k < this.nComponents; k++) {
+    gaussians.push(gaussian(this.means[k], this.vars[k]))
+  }
+  return gaussians
 }
--- test/gmm.test.js
+++ test/gmm.test.js
@@ -1,8 +1,43 @@
 var test = require('tap').test
 var GMM = require('../index')
 
+test('initialization', function (t) {
+  t.plan(2)
+  function f (k) {
+    return function () {
+      var gmm = new GMM(3, k)
+      gmm.sample(0)
+    }
+  }
+  t.throws(f([1, 2]))
+  t.doesNotThrow(f([1, 2, 3]))
+})
+
 test('random sample', function (t) {
-  var gmm = new GMM(3)
   t.plan(1)
+
+  var gmm = new GMM(3)
   t.equals(5, gmm.sample(5).length)
+})
+
+test('gaussians', function (t) {
+  t.plan(6)
+
+  var gmm = new GMM(3, [1 / 3, 1 / 3, 1 / 3], [0, 10, 20], [1, 2, 0.5])
+  var gaussians = gmm._gaussians()
+  t.equals(gaussians[0].mean, 0)
+  t.equals(gaussians[1].mean, 10)
+  t.equals(gaussians[2].mean, 20)
+  t.equals(gaussians[0].variance, 1)
+  t.equals(gaussians[1].variance, 2)
+  t.equals(gaussians[2].variance, 0.5)
+})
+
+test('scoring one data point', function (t) {
+  t.plan(3)
+
+  var gmm = new GMM(3, undefined, [0, 10, 20])
+  t.equals(gmm.scoreSample(5)[0], 0.5)
+  t.equals(gmm.scoreSample(5)[1], 0.5)
+  t.equals(gmm.scoreSample(0)[0], 1.0)
 })
